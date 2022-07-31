95b85485f3bb8bc98de3ebb8360b16accfd3bded
only <Mima@135>
2022-07-31 15:50:52 +0800

    http://116.62.62.54:9030/dl/lilishop-uniapp.git

---
+++ pages/mine/make/myMake.vue
@@ -0,0 +1,116 @@
+<template>
+	<view class="content">
+		<view class="make-box">
+			<div class="div-box">
+				<div class="div-box-text">
+					<view>通证</view>
+					<view class="div-box-view">{{ pointData.totalPoint || 2323233.23232 }}</view>
+				</div>
+				<div class="div-box-text">
+					<view>积分</view>
+					<view class="div-box-view">{{ pointData.point || 0 }}</view>
+				</div>
+			</div>
+		</view>
+		
+		<view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	// import { getPointsData } from "@/api/members.js";
+	// import { getMemberPointSum } from "@/api/members.js";
+	export default {
+		data() {
+			return {
+				count: {
+					loadStatus: "more",
+				},
+				pointList: [], //积分数据集合
+				params: {
+					pageNumber: 1,
+					pageSize: 10,
+				},
+				pointData: {}, //累计获取 未输入 集合
+			};
+		},
+
+		onLoad() {
+			this.initPointData();
+			this.getList();
+		},
+
+		/**
+		 * 触底加载
+		 */
+		onReachBottom() {
+			this.params.pageNumber++;
+			this.getList();
+		},
+		methods: {
+			/**
+			 * 获取积分数据
+			 */
+			// getList() {
+			//   let params = this.params;
+			//   uni.showLoading({
+			//     title: "加载中",
+			//   });
+			//   getPointsData(params).then((res) => {
+			//     uni.hideLoading();
+			//     if (res.data.success) {
+			//       let data = res.data.result.records;
+			//       if (data.length < 10) {
+			//         this.$set(this.count, "loadStatus", "noMore");
+			//         this.pointList.push(...data);
+			//       } else {
+			//         this.pointList.push(...data);
+			//         if (data.length < 10) this.$set(this.count, "loadStatus", "noMore");
+			//       }
+			//     }
+			//   });
+			// },
+
+			/**
+			 * 获得累计积分使用
+			 */
+			initPointData() {
+				getMemberPointSum().then((res) => {
+					this.pointData = res.data.result;
+				});
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		background: #f9f9f9;
+	}
+	.div-box{
+		display: flex;
+		position: relative;;
+	}
+	.div-box-text{
+		width: 300rpx;
+		text-align: center;
+	}
+	.div-box-view{
+		margin-top: 20rpx;
+	}
+	.make-box {
+		background-color: $main-color;
+		height: 250rpx;
+		background: linear-gradient(134deg, #28d094 2%, #1abc9c 98%);
+		border-radius: 20rpx 20rpx 20rpx 20rpx;
+		margin: 20rpx 20rpx 0;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: #ffffff;
+	}
+</style>
