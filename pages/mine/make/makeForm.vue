a8fcfb5eb0575d20ab769f5a22fd96b73389b605
Larry <1225346213@qq.com>
2022-08-06 13:18:09 +0800

    做单列表页面，编辑页面

---
+++ pages/mine/make/makeForm.vue
@@ -0,0 +1,196 @@
+<template>
+	<view class="container pb-120">
+		<view class="content">
+			<view class="form-group">
+				<view class="form-label">消费金额</view>
+				<input class="form-text" type="text" v-model="form.price" placeholder="金额"/>
+			</view>
+			<view class="form-group">
+				<view class="form-label">店铺</view>
+				<picker @change="bindPickerShopChange" :value="shopIndex" :range="shopNames">
+					<view class="form-text">{{ shopNames[shopIndex] }}</view>
+				</picker>
+				<view class="arrow-right"></view>
+			</view>
+			<view class="form-group">
+				<view class="form-label">让利比例</view>
+				<picker @change="bindPickerChange" :value="discountIndex" :range="discountRatioDescs">
+					<view class="form-text">{{ discountRatioDescs[discountIndex] }}</view>
+				</picker>
+				<view class="arrow-right"></view>
+			</view>
+			<view class="form-group">
+				<view class="form-label">让利金额</view>
+				<input class="form-text" type="text" v-model="form.discountPrice" placeholder="金额"/>
+			</view>
+			<view class="form-group">
+				<view class="form-label">手机号码</view>
+				<input class="form-text" type="text" v-model="form.tel" placeholder="输入会员手机号"/>
+			</view>
+			<view class="desc fs-28 mt-20">￥100.00/want</view>
+		</view>
+		
+		<view class="tip-info fs-28 mt-100">本次做单大约需要0(want)</view>
+		<view class="btn-submit" @click="handleSubmit">确认让利</view>
+		<u-keyboard @change="onChange" ref="uKeyboard" v-model="showKeyboard" @backspace="onBackspace" mode="number" :dot-enabled="false" :tooltip="false" default="">
+			<view class="mt-40 mb-40 text-center" style="text-align: center;">
+				请输入密码
+			</view>
+			<view class="mt-30 text-center fs-28">本次做单需要</view>
+			<view class="mt-15 text-center">
+				<text class="fs-48 font-weight-500" style="margin-right: 10rpx;">10</text>
+				<text>SSD</text>
+			</view>
+			<u-message-input class="mt-30" mode="box" :maxlength="6" :dot-fill="true" v-model="password" :disabled-keyboard="true" @finish="finish"></u-message-input>
+		</u-keyboard>
+	</view>
+</template>
+
+<script>
+	// import { getPointsData } from "@/api/members.js";
+	// import { getMemberPointSum } from "@/api/members.js";
+	export default {
+		data() {
+			return {
+				discountIndex: 0,
+				discountRatios: [1,2,3,4],
+				discountRatioDescs: ['1成','2成','3成','4成'],
+				shopIndex: 0,
+				shops: [1,2,3,4],
+				shopNames: ['店1','店2','店3','店4'],
+				form: {
+					price: 120,
+					shopId: 0,
+					discountRatio: "3折",
+					discountPrice: 200,
+					cellphone: 13311111111
+				},
+				showKeyboard: false,
+				password: ''
+			};
+		},
+		watch: {
+			discountIndex(oldVal, newVal) {
+				this.form.discountRatio = this.discountRatios[newVal];
+			},
+			shopIndex(oldVal, newVal) {
+				this.form.discountRatio = this.shops[newVal];
+			}
+		},
+		onLoad() {
+		},
+		onReachBottom() {
+			this.params.pageNumber++;
+			this.getList();
+		},
+		onBackspace(e) {
+			if (this.password.length > 0) {
+				this.password = this.password.substring(0, this.password.length - 1);
+			}
+		},
+		methods: {
+			handleSubmit() {
+				this.showKeyboard = true;
+				this.password='';
+			},
+			bindPickerChange(e) {
+				this.discountIndex = e.target.value
+			},
+			bindPickerShopChange(e) {
+				this.shopIndex = e.target.value
+			},
+			onChange(val) {
+				if (this.password.length < 6) {
+					this.password += val;
+				}
+
+				if (this.password.length >= 6) {
+					this.finish();//封装的结束函数，我是在后续还有请求接口和判断等
+				}
+			},
+			finish() {
+				this.showKeyboard = false;
+			}
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		width: 100%;
+		min-height: calc(100vh - 88rpx);
+		background-color: #ffffff;
+		
+		.content {
+			width: 100%;
+			padding: 20rpx 40rpx;
+		}
+		
+		.form-group {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			position: relative;
+			border-bottom: 1rpx $border-color-dark solid;
+			
+			.form-label {
+				width: 180rpx;
+				font-size: 32rpx;
+				font-family: PingFangSC-Semibold, PingFang SC;
+				font-weight: 600;
+				color: #000000;
+			}
+			
+			.form-text {
+				margin-left: 30rpx;
+				width: 504rpx;
+				height: 120rpx;
+				border-radius: 15rpx;
+				line-height: 120rpx;
+				font-size: 28rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #000000;
+				padding: 0 114rpx 0 24rpx;
+			}
+			
+			.arrow-right {
+				position: absolute;
+				right: 10rpx;
+				width: 30rpx;
+				height: 30rpx;
+				background-image: url("/static/icons/arrow-right.png");
+				background-size: cover;
+				background-repeat: no-repeat;
+			}
+		}
+		
+		
+		.desc {
+			text-align: right;
+			color: $main-color;
+		}
+		
+		.tip-info {
+			text-align: center;
+			color: $main-color;
+		}
+		
+		.btn-submit {
+			margin-left: 30rpx;
+			margin-top: 20rpx;
+			width: calc(100vw - 60rpx);
+			height: 100rpx;
+			background: #ff4a48;
+			font-size: 40rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #FFFFFF;
+			line-height: 100rpx;
+			border-radius: 50rpx;
+			text-align: center;
+		}
+		
+		
+	}
+</style>
--- pages/mine/make/myMake.vue
+++ pages/mine/make/myMake.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="container pb-120">
+	<view class="container">
 		<view class="mine-header">
 			<view>
 				<view>通证</view>
@@ -11,35 +11,40 @@
 			</view>
 		</view>
 		
-		<view class="card-area">
-			<view class="card-title">
-				做单记录
-			</view>
-			<view class="card-body">
-				<view v-for="(i, index) in 10" :key="i">
-					<view class="split-line-1 mt-20" v-if="index > 0"></view>
-					<view class="fs-30 d-flex justify-content-space-between" :class="index>0 ? 'mt-20' : ''">
-						<view>张三大杀四方</view>
-						<view>2000元</view>
-					</view>
-					<view class="mt-20 font-light d-flex justify-content-space-between">
-						<view>2021-11-11</view>
-						<view>17721212211</view>
+		<view  class="mine-content">
+			<view class="card-area">
+				<view class="card-title">
+					做单记录
+				</view>
+				<view class="card-body">
+					<view v-for="(i, index) in 15" :key="i">
+						<view class="split-line-1 mt-20" v-if="index > 0"></view>
+						<view class="fs-30 d-flex justify-content-space-between" :class="index>0 ? 'mt-20' : ''">
+							<view class="title">张三大杀四方{{ i }}</view>
+							<view>2000</view>
+						</view>
+						<view class="mt-20 font-light d-flex justify-content-space-between">
+							<view>2021-11-11</view>
+							<view>17721212211</view>
+						</view>
 					</view>
 				</view>
 			</view>
 		</view>
 		
-		<image src="/static/add.png" mode="" class="add-icon" @click="handleAdd"></image>
+		<view class="btn-bottom"  @click="handleAdd">
+			做单
+		</view>
 	</view>
 </template>
 
 <script>
-	// import { getPointsData } from "@/api/members.js";
-	// import { getMemberPointSum } from "@/api/members.js";
+	import { getPointsData, getMemberPointSum } from "@/api/members.js";
 	export default {
 		data() {
 			return {
+				loadStatus: "more",
+				pageNumber: 1,
 				count: {
 					loadStatus: "more",
 				},
@@ -68,25 +73,25 @@
 			/**
 			 * 获取积分数据
 			 */
-			// getList() {
-			//   let params = this.params;
-			//   uni.showLoading({
-			//     title: "加载中",
-			//   });
-			//   getPointsData(params).then((res) => {
-			//     uni.hideLoading();
-			//     if (res.data.success) {
-			//       let data = res.data.result.records;
-			//       if (data.length < 10) {
-			//         this.$set(this.count, "loadStatus", "noMore");
-			//         this.pointList.push(...data);
-			//       } else {
-			//         this.pointList.push(...data);
-			//         if (data.length < 10) this.$set(this.count, "loadStatus", "noMore");
-			//       }
-			//     }
-			//   });
-			// },
+			getList() {
+				let params = this.params;
+				uni.showLoading({
+					title: "加载中",
+				});
+				getPointsData(params).then((res) => {
+					uni.hideLoading();
+					if (res.data.success) {
+						let data = res.data.result.records;
+						if (data.length < 10) {
+							this.$set(this.count, "loadStatus", "noMore");
+							this.pointList.push(...data);
+						} else {
+							this.pointList.push(...data);
+							if (data.length < 10) this.$set(this.count, "loadStatus", "noMore");
+						}
+					}
+				});
+			},
 			
 			handleAdd() {
 				uni.navigateTo({
@@ -98,27 +103,59 @@
 			 * 获得累计积分使用
 			 */
 			initPointData() {
-				getMemberPointSum().then((res) => {
-					this.pointData = res.data.result;
-				});
+				// getMemberPointSum().then((res) => {
+				// 	this.pointData = res.data.result;
+				// });
 			},
 		},
 	};
 </script>
 
 <style lang="scss" scoped>
-@import "../mine-base.scss";
 	.container {
 		width: 100%;
 		
-		.add-icon {
+		.mine-header {
+		    background-image: url("/static/img/main-bg.png");
+		    background-size: cover;
+		    background-repeat: no-repeat;
+			border-radius: 0 0 30rpx 30rpx;
 			position: fixed;
-			bottom: 40rpx;
-			right: 20rpx;
-			width: 80rpx;
-			height: 80rpx;
-			background-color: rgba($color: #ffffff, $alpha: 0.8);
-			border-radius: 50%;
+			top: 88rpx;
+			left: 0;
+			right: 0;
+			display: flex;
+			align-items: center;
+			justify-content: space-around;
+			color: #FFFFFF;
+			height: 240rpx;
+			text-align: center;
+			z-index: 10;
+			box-shadow: 0px 4rpx 8rpx 0px rgba(255, 0, 0, 0.2);
+		}
+		
+		.mine-content {
+			margin-top: 268rpx;
+			padding-bottom: 180rpx;
+			
+			.title {
+				font-weight: 900;
+			}
+		}
+		
+		.btn-bottom {
+			position: fixed;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			height: 120rpx;
+			line-height: 120rpx;
+			background: #ff4a48;
+			font-size: 20px;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #FFFFFF;
+			text-align: center;
 		}
 	}
 </style>
