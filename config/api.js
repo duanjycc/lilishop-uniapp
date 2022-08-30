c0dc59865deebb5e106662c315dce20c002cee6b
Larry <1225346213@qq.com>
2022-08-30 11:03:20 +0800

    设置支付密码

--- config/api.js
+++ config/api.js
@@ -4,11 +4,11 @@
  */
 // 开发环境
 const dev = {
-	// common: "https://wang.wantbibi.com/common-api",
-	// buyer: "https://wang.wantbibi.com/buyer-api",
+	common: "https://wang.wantbibi.com/common-api",
+	buyer: "https://wang.wantbibi.com/buyer-api",
  
-	common: "http://8.136.107.94:7003",
-	buyer: "http://8.136.107.94:7002",
+	// common: "http://8.136.107.94:7003",
+	// buyer: "http://8.136.107.94:7002",
  
 	// common: "http://192.168.0.106:8890",
 	// buyer: "http://192.168.0.101:8888",
