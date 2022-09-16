3f48b97b5322a28bdf3d21f4ba5b7b7b14620be4
only <Mima@135>
2022-09-16 14:59:31 +0800

    32

--- config/api.js
+++ config/api.js
@@ -5,13 +5,7 @@
 // 开发环境
 const dev = {
 	common: "https://wang.wantbibi.com/common-api",
-	// buyer: "https://wang.wantbibi.com/buyer-api",
- 
-	// common: "http://8.136.107.94:7003",
-	buyer: "http://8.136.107.94:7002",
- 
-	// common: "http://192.168.0.106:8890",
-	// buyer: "http://192.168.0.103:8888",
+  buyer: "https://wang.wantbibi.com/buyer-api",
 };
 // 生产环境
 const prod = {
