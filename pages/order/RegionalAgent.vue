c0da4e2e0c406ab094f6f45d8616fdb8cb5ba858
duanjycc <312311597@qq.com>
2022-11-26 19:30:43 +0800

    1

--- pages/order/RegionalAgent.vue
+++ pages/order/RegionalAgent.vue
@@ -56,6 +56,7 @@
 		methods: {
 			submitSearchRegionList(){
 				this.params.pageNumber = 1;
+				this.pages = 1;
 				this.serviceProviderList = [];
 				this.getData();
 			},
@@ -65,6 +66,7 @@
 			onPullDownRefresh() {
 				//下拉刷新
 				this.params.pageNumber = 1;
+				this.pages = 1;
 				this.serviceProviderList = [];
 				this.getData();
 			},
